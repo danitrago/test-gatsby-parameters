b543ec270104455479b3801ea9a9f9b9c5becd21
danitrago <danitrago.7@gmail.com>
2022-05-25 10:34:49 -0500

    demo

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,33 +1,241 @@
-const path = require(`path`)
+const path = require(`path`);
+
+const data = {
+  status: 200,
+  userMessage: "",
+  developerMessage: "",
+  errorCode: null,
+  moreInfo: null,
+  data: {
+    area: [
+      "Administración y Secretarías",
+      "Agropecuario, Pesca y Forestal",
+      "Artes y Diseño",
+      "Comunicación, Periodismo y Relaciones Públicas",
+      "Recursos Humanos",
+      "Venta y Post Venta -  Personas (B2C)"
+    ],
+    jornadaLaboral: [
+      "Full Time",
+      "Part Time",
+      "Por horas",
+      "Por turnos",
+      "Práctica / Pasantía",
+      "TEMPORARY"
+    ],
+    ciudad: [
+      { mun: "Medellín", region: "Medellín y área metropolitana" },
+      { mun: "Abejorral", region: "Oriente" },
+      { mun: "Abriaquí", region: "Occidente" },
+      { mun: "Alejandría", region: "Oriente" },
+      { mun: "Amagá", region: "Suroeste" },
+      { mun: "Amalfi", region: "Nordeste" },
+      { mun: "Andes", region: "Suroeste" },
+      { mun: "Angelópolis", region: "Suroeste" },
+      { mun: "Angostura", region: "Norte" },
+      { mun: "Anorí", region: "Nordeste" },
+      { mun: "Anza", region: "Occidente" },
+      { mun: "Apartadó", region: "Urabá" },
+      { mun: "Arboletes", region: "Urabá" },
+      { mun: "Argelia", region: "Oriente" },
+      { mun: "Armenia", region: "Occidente" },
+      { mun: "Barbosa", region: "Medellín y área metropolitana" },
+      { mun: "Bello", region: "Medellín y área metropolitana" },
+      { mun: "Belmira", region: "Norte" },
+      { mun: "Betania", region: "Suroeste" },
+      { mun: "Betulia", region: "Suroeste" },
+      { mun: "Briceño", region: "Norte" },
+      { mun: "Buriticá", region: "Occidente" },
+      { mun: "Cáceres", region: "Bajo Cauca" },
+      { mun: "Caicedo", region: "Occidente" },
+      { mun: "Caldas", region: "Medellín y área metropolitana" },
+      { mun: "Campamento", region: "Norte" },
+      { mun: "Cañasgordas", region: "Occidente" },
+      { mun: "Caracolí", region: "Magdalena Medio" },
+      { mun: "Caramanta", region: "Suroeste" },
+      { mun: "Carepa", region: "Urabá" },
+      { mun: "Carolina", region: "Medellín y área metropolitana" },
+      { mun: "Caucasia", region: "Bajo Cauca" },
+      { mun: "Chigorodó", region: "Urabá" },
+      { mun: "Cisneros", region: "Nordeste" },
+      { mun: "Ciudad Bolívar", region: "Suroeste" },
+      { mun: "Cocorná", region: "Oriente" },
+      { mun: "Concepción", region: "Oriente" },
+      { mun: "Concordia", region: "Suroeste" },
+      { mun: "Copacabana", region: "Medellín y área metropolitana" },
+      { mun: "Dabeiba", region: "Occidente" },
+      { mun: "Don Matías", region: "Norte" },
+      { mun: "Ebéjico", region: "Occidente" },
+      { mun: "El Bagre", region: "Bajo Cauca" },
+      { mun: "El Carmen de Viboral", region: "Oriente" },
+      { mun: "El Peñol", region: "Oriente" },
+      { mun: "El Retiro", region: "Oriente" },
+      { mun: "El Santuario", region: "Oriente" },
+      { mun: "Entrerrios", region: "Norte" },
+      { mun: "Envigado", region: "Medellín y área metropolitana" },
+      { mun: "Fredonia", region: "Suroeste" },
+      { mun: "Frontino", region: "Occidente" },
+      { mun: "Giraldo", region: "Occidente" },
+      { mun: "Girardota", region: "Medellín y área metropolitana" },
+      { mun: "Gómez Plata", region: "Norte" },
+      { mun: "Granada", region: "Oriente" },
+      { mun: "Guadalupe", region: "Norte" },
+      { mun: "Guarne", region: "Oriente" },
+      { mun: "Guatapé", region: "Oriente" },
+      { mun: "Heliconia", region: "Occidente" },
+      { mun: "Hispania", region: "Suroeste" },
+      { mun: "Itagui", region: "Medellín y área metropolitana" },
+      { mun: "Ituango", region: "Norte" },
+      { mun: "Jardín", region: "Suroeste" },
+      { mun: "Jericó", region: "Suroeste" },
+      { mun: "La Ceja", region: "Oriente" },
+      { mun: "La Estrella", region: "Medellín y área metropolitana" },
+      { mun: "La Pintada", region: "Suroeste" },
+      { mun: "La Unión", region: "Oriente" },
+      { mun: "Liborina", region: "Occidente" },
+      { mun: "Maceo", region: "Magdalena Medio" },
+      { mun: "Marinilla", region: "Oriente" },
+      { mun: "Montebello", region: "Suroeste" },
+      { mun: "Murindó", region: "Urabá" },
+      { mun: "Mutatá", region: "Urabá" },
+      { mun: "Nariño", region: "Oriente" },
+      { mun: "Nechí", region: "Bajo Cauca" },
+      { mun: "Necoclí", region: "Urabá" },
+      { mun: "Olaya", region: "Occidente" },
+      { mun: "Peque", region: "Occidente" },
+      { mun: "Pueblorrico", region: "Suroeste" },
+      { mun: "Puerto Berrío", region: "Magdalena Medio" },
+      { mun: "Puerto Nare", region: "Magdalena Medio" },
+      { mun: "Puerto Triunfo", region: "Magdalena Medio" },
+      { mun: "Remedios", region: "Nordeste" },
+      { mun: "Rionegro", region: "Medellín y área metropolitana" },
+      { mun: "Sabanalarga", region: "Occidente" },
+      { mun: "Sabaneta", region: "Medellín y área metropolitana" },
+      { mun: "Salgar", region: "Suroeste" },
+      { mun: "San Andrés de Cuerquía", region: "Norte" },
+      { mun: "San Carlos", region: "Oriente" },
+      { mun: "San Francisco", region: "Oriente" },
+      { mun: "San Jerónimo", region: "Occidente" },
+      { mun: "San José de La Montaña", region: "Norte" },
+      { mun: "San Juan de Urabá", region: "Urabá" },
+      { mun: "San Luis", region: "Oriente" },
+      { mun: "San Pedro", region: "Norte" },
+      { mun: "San Pedro de Uraba", region: "Urabá" },
+      { mun: "San Rafael", region: "Oriente" },
+      { mun: "San Roque", region: "Nordeste" },
+      { mun: "San Vicente", region: "Oriente" },
+      { mun: "Santa Bárbara", region: "Suroeste" },
+      { mun: "Santa Rosa de Osos", region: "Norte" },
+      { mun: "Santafé de Antioquia", region: "Occidente" },
+      { mun: "Santo Domingo", region: "Nordeste" },
+      { mun: "Segovia", region: "Nordeste" },
+      { mun: "Sonson", region: "Oriente" },
+      { mun: "Sopetrán", region: "Occidente" },
+      { mun: "Támesis", region: "Suroeste" },
+      { mun: "Tarazá", region: "Bajo Cauca" },
+      { mun: "Tarso", region: "Suroeste" },
+      { mun: "Titiribí", region: "Suroeste" },
+      { mun: "Toledo", region: "Norte" },
+      { mun: "Turbo", region: "Urabá" },
+      { mun: "Uramita", region: "Occidente" },
+      { mun: "Urrao", region: "Suroeste" },
+      { mun: "Valdivia", region: "Norte" },
+      { mun: "Valparaíso", region: "Suroeste" },
+      { mun: "Vegachí", region: "Nordeste" },
+      { mun: "Venecia", region: "Suroeste" },
+      { mun: "Vigía del Fuerte", region: "Urabá" },
+      { mun: "Yalí", region: "Nordeste" },
+      { mun: "Yarumal", region: "Norte" },
+      { mun: "Yolombó", region: "Nordeste" },
+      { mun: "Yondó", region: "Magdalena Medio" },
+      { mun: "Zaragoza", region: "Bajo Cauca" }
+    ],
+    modalidadRemoto: [
+      "Remoto solo localmente",
+      "Sin opción remoto",
+      "Temporalmente remoto",
+      "Totalmente remoto"
+    ],
+    tipoContrato: ["Otro", "Permanente (Indefinido)", "Temporal (Plazo Fijo)"],
+    region: [
+      "Bajo Cauca",
+      "Magdalena Medio",
+      "Medellín y área metropolitana",
+      "Nordeste",
+      "Norte",
+      "Occidente",
+      "Oriente",
+      "Suroeste",
+      "Urabá"
+    ]
+  }
+};
+
 // Log out information after a build is done
 exports.onPostBuild = ({ reporter }) => {
-  reporter.info(`Your Gatsby site has been built!`)
-}
+  reporter.info(`Your Gatsby site has been built!`);
+};
 // Create blog pages dynamically
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const Vacantes = path.resolve(`src/templates/Vacantes.tsx`)
+  const { createPage } = actions;
+  const Vacantes = path.resolve(`src/templates/Vacantes.tsx`);
+  const RegionTemplate = path.resolve(`src/templates/RegionTemplate.tsx`);
+  const MunicipioTemplate = path.resolve(`src/templates/MunicipioTemplate.tsx`);
   const result = [
-      {
-          slug: '/vacantesV3/municipio/Rionegro',
-          title: 'Rionegro'
-      },
-      {
-        slug: '/vacantesV3/municipio/La Ceja',
-        title: 'La Ceja'
+    {
+      slug: "/vacantesV3/municipio/Rionegro",
+      title: "Rionegro"
     },
     {
-        slug: '/vacantesV3/municipio/Marinilla',
-        title: 'Marinilla'
+      slug: "/vacantesV3/municipio/La Ceja",
+      title: "La Ceja"
     },
-  ]
-  result.forEach(edge => {
+    {
+      slug: "/vacantesV3/municipio/Marinilla",
+      title: "Marinilla"
+    },
+    {
+      slug: "/vacantesV3/municipio/Abejorral",
+      title: "Abejorral"
+    },
+    {
+      slug: "/vacantesV3/municipio/Itaguí",
+      title: "Itaguí"
+    }
+  ];
+
+  result.forEach((edge) => {
     createPage({
       path: `${edge.slug}`,
       component: Vacantes,
       context: {
-        title: edge.title,
-      },
-    })
-  })
-}
+        title: edge.title
+      }
+    });
+  });
+
+  data.data.region.forEach((region) => {
+    createPage({
+      path: `/region/${region}`,
+      component: RegionTemplate,
+      context: {
+        title: region
+      }
+    });
+  });
+
+  data.data.ciudad.forEach((ciudad) => {
+    createPage({
+      path: `/region/${ciudad.region}/${ciudad.mun}`,
+      component: MunicipioTemplate,
+      context: {
+        municipio: ciudad.mun,
+        region: ciudad.region
+      }
+    });
+  });
+
+
+
+
+};
---
+++ src/templates/RegionTemplate.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { StringParam, useQueryParam } from "use-query-params";
+import { Link } from "gatsby";
+
+const data = [
+  {
+    id: 0,
+    name: "Auxiliar de enfermería",
+    cat: "Salud",
+    mun: "Rionegro"
+  },
+  {
+    id: 1,
+    name: "Auxiliar contable",
+    cat: "Administración",
+    mun: "La Ceja"
+  },
+  {
+    id: 2,
+    name: "Desarrollador backend",
+    cat: "Tecnología",
+    mun: "Marinilla"
+  },
+  {
+    id: 3,
+    name: "Maquetador HTML",
+    cat: "Tecnología",
+    mun: "Rionegro"
+  }
+];
+
+const unique = (value: any, index: any, self: any) => {
+  return self.indexOf(value) === index;
+};
+
+const RegionTemplate = (props: any) => {
+  console.log(props);
+  const categorias = data.map((vacante) => vacante.cat).filter(unique);
+  const municipios = data.map((vacante) => vacante.mun).filter(unique);
+
+  // something like: ?cat=cat2&mun=mun3 in the URL
+  const [cat, setCat] = useQueryParam<any>("Categoría", StringParam);
+  const [mun, setMun] = useQueryParam<any>("Municipio", StringParam);
+
+  const dataFiltered = data.filter(
+    (vacante) =>
+      vacante.cat.includes(cat || "") && vacante.mun.includes(props.pageContext.title)
+  );
+
+  return (
+    <div>
+      <h3>Vacantes, Servicios de Empleo</h3>
+      <hr />
+      <div style={{ display: "flex" }}>
+        <div>
+          <h3>Filtros</h3>
+          <h4>Categorías</h4>
+          {categorias.map((item) => (
+            <label key={item}>
+              <input
+                type="checkbox"
+                value={item}
+                onChange={() => setCat(item)}
+                checked={item === cat ? true : false}
+              />
+              {item} <br />
+            </label>
+          ))}
+          <h4>Municipios</h4>
+          {municipios.map((item) => (
+            <p>
+              <Link to={`/vacantesV3/municipio/${item}`}>{item}</Link>
+            </p>
+          ))}
+        </div>
+        <div>
+          <h1>
+            Vacantes Disponibles en la Región: {props.pageContext.title}{" "}
+            {cat ? `en la Categoría de ${cat}` : null}
+          </h1>
+          <ul>
+            {dataFiltered.length > 0
+              ? dataFiltered.map((vacante) => (
+                  <li key={vacante.id} style={{ marginBottom: 20 }}>
+                    <b>{vacante.name}</b> <br />
+                    <small>{vacante.cat + " --- En: " + vacante.mun}</small>
+                  </li>
+                ))
+              : "No hay vacantes disponibles para tu búsqueda."}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegionTemplate;
